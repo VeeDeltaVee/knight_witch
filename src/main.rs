5d470ade0abc2f46426b055fd8f7bde176e15416
VeeDeltaVee <45564258+VeeDeltaVee@users.noreply.github.com>
2020-11-01 20:30:45 +0530

    Initialized project

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+struct BoardState {
+    // Representation of the board in Little Endian Rank File Mapping
+    // See chessprogramming wiki for details
+    board: [u8; 63],
+}
+
+fn main() {
+    println!("Hello, world!");
+}
