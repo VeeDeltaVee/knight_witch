f72bdedba96d4a4067e6f9ef3910d8a1c7903c43
Versha Dhankar <veedeltavee@gmail.com>
2023-10-21 16:09:55 -0700

    Moved all knight move generation code Knight move generation code now also has it's own submodule

--- src/board.rs
+++ src/board.rs
@@ -1,8 +1,11 @@
+pub mod knight;
 pub mod pawn;
 mod test_utils;
 
 use crate::board::pawn::PawnMovement;
 use std::fmt;
+
+use self::knight::KnightMovement;
 
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub enum PieceType {
@@ -245,50 +248,6 @@ impl Board {
         }
     }
 
-    fn generate_knight_moves(&self) -> Result<Vec<Board>, &'static str> {
-        let jumps = vec![
-            (-1, 2),
-            (1, 2),
-            (-2, 1),
-            (2, 1),
-            (-2, -1),
-            (2, -1),
-            (-1, -2),
-            (1, -2),
-        ];
-
-        let knight_positions = self.get_positions_of_pieces_with_given_side_and_type(
-            PieceType::Knight,
-            PieceSide::CurrentlyMoving,
-        )?;
-
-        let mut possible_boards = vec![];
-        for old_pos in knight_positions {
-            let new_boards = jumps
-                .iter()
-                .map(|(file, rank)| Direction {
-                    file: *file,
-                    rank: *rank,
-                })
-                // Get target square and check for out-of-bounds moves
-                .filter_map(|dir| self.add_direction_to_position(old_pos, dir).ok())
-                // Check target square: can't take own pieces
-                .filter(
-                    |new_pos| match self.get_piece_at_position(*new_pos).unwrap() {
-                        None => true,
-                        Some((_, PieceSide::MovingNext)) => true,
-                        Some((_, PieceSide::CurrentlyMoving)) => false,
-                    },
-                )
-                // Should be able to move there without error
-                .filter_map(|new_pos| self.new_board_with_moved_piece(old_pos, new_pos).ok());
-
-            possible_boards.extend(new_boards);
-        }
-
-        Ok(possible_boards)
-    }
-
     // TODO: Rest of move impls
     fn generate_bishop_moves(&self) -> Vec<Board> {
         vec![]
@@ -474,106 +433,6 @@ mod test {
             for i in 48..64 {
                 assert!(matches!(board.squares[i], Some((_, PieceSide::MovingNext))));
             }
-        }
-    }
-
-    mod knight_moves {
-
-        use super::*;
-
-        // Returns a board with the setup
-        // P.....
-        // ...p..
-        // .K..p.
-        // ...P.p
-        // ...K..
-        // .....P
-        fn get_board_for_simple_knight_moves() -> Board {
-            let mut board = Board::with_pieces(vec![None; 6 * 6], 6);
-
-            board
-                .set_piece_at_position(
-                    Some((PieceType::Knight, PieceSide::CurrentlyMoving)),
-                    Square { rank: 1, file: 3 },
-                )
-                .unwrap();
-            board
-                .set_piece_at_position(
-                    Some((PieceType::Knight, PieceSide::CurrentlyMoving)),
-                    Square { rank: 3, file: 1 },
-                )
-                .unwrap();
-
-            board
-                .set_piece_at_position(
-                    Some((PieceType::Pawn, PieceSide::CurrentlyMoving)),
-                    Square { rank: 2, file: 3 },
-                )
-                .unwrap();
-            board
-                .set_piece_at_position(
-                    Some((PieceType::Pawn, PieceSide::CurrentlyMoving)),
-                    Square { rank: 0, file: 5 },
-                )
-                .unwrap();
-            board
-                .set_piece_at_position(
-                    Some((PieceType::Pawn, PieceSide::CurrentlyMoving)),
-                    Square { rank: 5, file: 0 },
-                )
-                .unwrap();
-
-            board
-                .set_piece_at_position(
-                    Some((PieceType::Pawn, PieceSide::MovingNext)),
-                    Square { rank: 2, file: 5 },
-                )
-                .unwrap();
-            board
-                .set_piece_at_position(
-                    Some((PieceType::Pawn, PieceSide::MovingNext)),
-                    Square { rank: 3, file: 4 },
-                )
-                .unwrap();
-            board
-                .set_piece_at_position(
-                    Some((PieceType::Pawn, PieceSide::MovingNext)),
-                    Square { rank: 4, file: 3 },
-                )
-                .unwrap();
-
-            board
-        }
-
-        #[test]
-        fn moves_like_a_knight() {
-            let board = get_board_for_simple_knight_moves();
-
-            let moved_boards = board.generate_moves().unwrap();
-
-            let expected_moves = vec![
-                Square { rank: 5, file: 2 },
-                Square { rank: 4, file: 3 },
-                Square { rank: 1, file: 2 },
-                Square { rank: 1, file: 0 },
-                Square { rank: 3, file: 2 },
-                Square { rank: 3, file: 4 },
-                Square { rank: 2, file: 1 },
-                Square { rank: 2, file: 5 },
-                Square { rank: 0, file: 1 },
-            ];
-            let unexpected_moves = vec![
-                Square { rank: 5, file: 0 },
-                Square { rank: 0, file: 5 },
-                Square { rank: 2, file: 3 },
-            ];
-
-            check_for_moves(
-                moved_boards,
-                expected_moves,
-                unexpected_moves,
-                Some((PieceType::Knight, PieceSide::CurrentlyMoving)),
-            );
         }
     }
 
---
+++ src/board/knight.rs
@@ -0,0 +1,156 @@
+use crate::board::Board;
+
+use super::{Direction, PieceSide, PieceType, Square};
+
+pub trait KnightMovement {
+    fn generate_knight_moves(&self) -> Result<Vec<Self>, &'static str>
+    where
+        Self: Sized;
+}
+
+impl KnightMovement for Board {
+    fn generate_knight_moves(&self) -> Result<Vec<Board>, &'static str> {
+        let jumps = vec![
+            (-1, 2),
+            (1, 2),
+            (-2, 1),
+            (2, 1),
+            (-2, -1),
+            (2, -1),
+            (-1, -2),
+            (1, -2),
+        ];
+
+        let knight_positions = self.get_positions_of_pieces_with_given_side_and_type(
+            PieceType::Knight,
+            PieceSide::CurrentlyMoving,
+        )?;
+
+        let mut possible_boards = vec![];
+        for old_pos in knight_positions {
+            let new_boards = jumps
+                .iter()
+                .map(|(file, rank)| Direction {
+                    file: *file,
+                    rank: *rank,
+                })
+                // Get target square and check for out-of-bounds moves
+                .filter_map(|dir| self.add_direction_to_position(old_pos, dir).ok())
+                // Check target square: can't take own pieces
+                .filter(
+                    |new_pos| match self.get_piece_at_position(*new_pos).unwrap() {
+                        None => true,
+                        Some((_, PieceSide::MovingNext)) => true,
+                        Some((_, PieceSide::CurrentlyMoving)) => false,
+                    },
+                )
+                // Should be able to move there without error
+                .filter_map(|new_pos| self.new_board_with_moved_piece(old_pos, new_pos).ok());
+
+            possible_boards.extend(new_boards);
+        }
+
+        Ok(possible_boards)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::board::test_utils::check_for_moves;
+
+    // Returns a board with the setup
+    // P.....
+    // ...p..
+    // .K..p.
+    // ...P.p
+    // ...K..
+    // .....P
+    fn get_board_for_simple_knight_moves() -> Board {
+        let mut board = Board::with_pieces(vec![None; 6 * 6], 6);
+
+        board
+            .set_piece_at_position(
+                Some((PieceType::Knight, PieceSide::CurrentlyMoving)),
+                Square { rank: 1, file: 3 },
+            )
+            .unwrap();
+        board
+            .set_piece_at_position(
+                Some((PieceType::Knight, PieceSide::CurrentlyMoving)),
+                Square { rank: 3, file: 1 },
+            )
+            .unwrap();
+
+        board
+            .set_piece_at_position(
+                Some((PieceType::Pawn, PieceSide::CurrentlyMoving)),
+                Square { rank: 2, file: 3 },
+            )
+            .unwrap();
+        board
+            .set_piece_at_position(
+                Some((PieceType::Pawn, PieceSide::CurrentlyMoving)),
+                Square { rank: 0, file: 5 },
+            )
+            .unwrap();
+        board
+            .set_piece_at_position(
+                Some((PieceType::Pawn, PieceSide::CurrentlyMoving)),
+                Square { rank: 5, file: 0 },
+            )
+            .unwrap();
+
+        board
+            .set_piece_at_position(
+                Some((PieceType::Pawn, PieceSide::MovingNext)),
+                Square { rank: 2, file: 5 },
+            )
+            .unwrap();
+        board
+            .set_piece_at_position(
+                Some((PieceType::Pawn, PieceSide::MovingNext)),
+                Square { rank: 3, file: 4 },
+            )
+            .unwrap();
+        board
+            .set_piece_at_position(
+                Some((PieceType::Pawn, PieceSide::MovingNext)),
+                Square { rank: 4, file: 3 },
+            )
+            .unwrap();
+
+        board
+    }
+
+    #[test]
+    fn moves_like_a_knight() {
+        let board = get_board_for_simple_knight_moves();
+
+        let moved_boards = board.generate_moves().unwrap();
+
+        let expected_moves = vec![
+            Square { rank: 5, file: 2 },
+            Square { rank: 4, file: 3 },
+            Square { rank: 1, file: 2 },
+            Square { rank: 1, file: 0 },
+            Square { rank: 3, file: 2 },
+            Square { rank: 3, file: 4 },
+            Square { rank: 2, file: 1 },
+            Square { rank: 2, file: 5 },
+            Square { rank: 0, file: 1 },
+        ];
+        let unexpected_moves = vec![
+            Square { rank: 5, file: 0 },
+            Square { rank: 0, file: 5 },
+            Square { rank: 2, file: 3 },
+        ];
+
+        check_for_moves(
+            moved_boards,
+            expected_moves,
+            unexpected_moves,
+            Some((PieceType::Knight, PieceSide::CurrentlyMoving)),
+        );
+    }
+}
