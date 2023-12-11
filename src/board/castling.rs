dd06b2888e6a9b51a2574babe016b0223cd206e8
Versha Dhankar <veedeltavee@gmail.com>
2023-12-10 19:04:31 -0800

    feat(castling): implement public castling interface
    Castling is a relatively big feature. It requires adding:
    1. State to track castling
    2. Updating the state as moves happen, so that castling is disallowed
       wherever a rook or king moves
    3. Actually figuring out the castling move
    
    Also, castling involves checking for threat a lot more than other moves,
    since it can't be done if the king is currently in check, and it also
    can't be done if any of the squares the king would move over are
    threatened.
    
    I'm splitting this work into multiple commits. This commit implements
    the public facing interface of generating castling moves, but with all
    the private implementation left todo!()
    
    This also hasn't started implementing state updates

--- src/board/castling.rs
+++ src/board/castling.rs
@@ -1,6 +1,6 @@
 use super::{Side, Board};
 
-
+#[derive(Clone, Copy)]
 pub enum CastlingDirection {
     Kingside,
     Queenside
@@ -44,6 +44,108 @@ fn calculate_index(side: Side, direction: CastlingDirection) -> usize {
     };
 
     side_index
+}
+
+pub trait CastlingMovement {
+    fn generate_castling_moves(&self, checked: bool) -> Result<Vec<Self>, &'static str>
+    where
+        Self: Sized;
+}
+
+impl CastlingMovement for Board {
+    /// Implementation of castling movement for Board.
+    /// Assumes that the game is being played on a standard
+    /// board with default configuration, so for now doesn't
+    /// support stuff like 960 or odd sized boards
+    fn generate_castling_moves(&self, checked: bool) -> Result<Vec<Self>, &'static str> {
+        let can_castle_anywhere = self.can_castle_any()?;
+        if !can_castle_anywhere {
+            return Ok(vec![]);
+        }
+
+        self.ensure_king_position();
+
+        if self.check_king_threat()? {
+            return OK(vec![])
+        }
+
+        let moves = vec![];
+
+        let directions = [CastlingDirection::Queenside, CastlingDirection::Kingside];
+        for dir in directions {
+            if !self.can_castle(self.current_move, dir) {
+                continue;
+            }
+
+            if self.are_pieces_blocking(dir)? {
+                continue;
+            }
+
+            let boards = self.moving_king_boards(dir)?;
+
+            // This looks kind of weird because Rust doesn't really have a good
+            // way to turn a Result of Iter into an Iter of Result. It involves
+            // `collect()`ing along the way, which means an extra allocation we
+            // don't need.
+            //
+            // This is basically doing the following:
+            // boards.map(is_threat).all(is false)
+            let is_king_threatened_on_the_way = boards
+                .iter()
+                .map(|board| board.check_king_threat())
+                .try_fold(
+                    false, |any_in_threat, threat_result| threat_result.map(
+                        |threat| any_in_threat | threat
+                    ))?;
+
+            if is_king_threatened_on_the_way {
+                continue;
+            }
+
+            moves.push(self.castle(dir)?);
+        }
+
+        Ok(moves)
+    }
+}
+
+trait CastlingMovementImpl {
+    fn ensure_king_position(&self) -> Result<(), &'static str>;
+    fn ensure_rook_position(&self, dir: CastlingDirection) -> Result<(), &'static str>;
+    fn can_castle_any(&self) -> Result<bool, &'static str>;
+    fn are_pieces_blocking(&self, dir: CastlingDirection) -> Result<bool, &'static str>;
+    fn moving_king_boards(&self, dir: CastlingDirection) -> Result<Vec<Self>, &'static str>
+    where
+        Self: Sized;
+    fn castle(&self, dir: CastlingDirection) -> Result<Self, &'static str>
+    where
+        Self: Sized;
+}
+
+impl CastlingMovementImpl for Board {
+    fn ensure_king_position(&self) -> Result<(), &'static str> {
+        todo!()
+    }
+
+    fn ensure_rook_position(&self, dir: CastlingDirection) -> Result<(), &'static str> {
+        todo!()
+    }
+
+    fn can_castle_any(&self) -> Result<bool, &'static str> {
+        todo!()
+    }
+
+    fn are_pieces_blocking(&self, dir: CastlingDirection) -> Result<bool, &'static str> {
+        todo!()
+    }
+
+    fn moving_king_boards(&self, dir: CastlingDirection) -> Result<Vec<Self>, &'static str> {
+        todo!()
+    }
+
+    fn castle(&self, dir: CastlingDirection) -> Result<Self, &'static str> {
+        todo!()
+    }
 }
 
 #[cfg(test)]
